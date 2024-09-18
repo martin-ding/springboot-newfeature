8b0cf4999e54a0aa36ec33dd2c3cbfc73b1763a7
dingmac <1150354658@qq.com>
2024-09-18 14:49:10 +0800

    init

---
+++ src/main/java/com/local/localdemo/LocaldemoApplication.java
@@ -0,0 +1,13 @@
+package com.local.localdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaldemoApplication.class, args);
+	}
+
+}
